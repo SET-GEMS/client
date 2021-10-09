abf444785af40f9b29cbbcc9466cab9ae7b2830f
ParkJeongseon <wjdtjs529@gmail.com>
2021-10-09 19:16:30 +0900

    Close #29: Add service worker

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Welcome from "./pages/Welcome";
 import Single from "./pages/Single";
 import Multi from "./pages/Multi";
 import { SINGLE, MULTI } from "./constants/mode";
+import Loading from "./components/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [mode, setMode] = useState("");
   const handleHomeButtonClick = () => setMode("");
 
+  useEffect(() => {
+    async function registerServiceWorker() {
+      if ("serviceWorker" in navigator) {
+        await navigator.serviceWorker.register("/sw.js");
+      }
+
+      setIsLoading(false);
+    }
+
+    registerServiceWorker();
+  }, []);
+
   return (
     <div className="App">
-      {!mode && <Welcome onSelectMode={setMode} />}
-      {mode === SINGLE
-        && <Single onHomeButtonClick={handleHomeButtonClick} />}
-      {mode === MULTI
-        && <Multi onHomeButtonClick={handleHomeButtonClick} />}
+      {isLoading ? <Loading /> : <div>
+        {!mode && <Welcome onSelectMode={setMode} />}
+        {mode === SINGLE
+          && <Single onHomeButtonClick={handleHomeButtonClick} />}
+        {mode === MULTI
+          && <Multi onHomeButtonClick={handleHomeButtonClick} />}
+      </div>}
     </div>
   );
 }
